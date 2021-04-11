164c1275721c18530aa181870a2abd48c6b89413
Svitlana Bondarenko <svogk@ua.fm>
2021-04-11 14:41:11 +0300

    add component Materials, edit UsefulInfoView

---
+++ src/components/Materials/Materials.js
@@ -0,0 +1,51 @@
+import books from '../../assets/materials/literature.json';
+import resources from '../../assets/materials/resources.json';
+
+import s from './Materials.module.css';
+
+export default function Materials() {
+  return (
+    <div className={s.useContainer}>
+      <div className={s.usefulInfo}>
+        <h2 className={s.title}>Useful literature</h2>
+        <ol className={s.list}>
+          {books.map(book => {
+            return (
+              <li className={s.item} key={book.id}>
+                <a
+                  className={`${s.itemLink} ${s.book}`}
+                  href={book.url}
+                  aria-label="book"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {`"${book.name}", ${book.author}`}
+                </a>
+              </li>
+            );
+          })}
+        </ol>
+      </div>
+
+      <div className={s.usefulInfo}>
+        <h2 className={s.title}>Useful resources</h2>
+        <ol className={s.list}>
+          {resources.map(resource => {
+            return (
+              <li className={s.item} key={resource.id}>
+                <a
+                  className={s.itemLink}
+                  href={resource.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {resource.description}
+                </a>
+              </li>
+            );
+          })}
+        </ol>
+      </div>
+    </div>
+  );
+}
